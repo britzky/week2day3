b1db91427c95418b1482318c9a7447aed45a913a
Britzky <johnclapper89@gmail.com>
2023-02-09 09:02:01 -0500

    first commit

---
+++ house_and_circle.py
@@ -0,0 +1,11 @@
+def square_foot_house(length, width):
+    '''
+    Function to show the square footage of a house (length x width = area) 
+    '''
+    return length * width
+
+def circumference_of_circle(diameter, pi=3.14):
+        '''pi='3.14', 
+        Function to show the circumference of a circle (pi * diammeter = circumference) 
+        '''
+        return diameter * pi
